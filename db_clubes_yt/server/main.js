758dfdb93204a25cb79653fdf1537c5d688e0f5f
Valerylu97 <65468343+Valerylu97@users.noreply.github.com>
2021-07-18 19:35:03 -0500

    commit cccyt

--- db_clubes_yt/server/main.js
+++ db_clubes_yt/server/main.js
@@ -4,7 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import  { Club_Mentores, Miembros_Mentores, Actividad_Mentores } from '/imports/api/db_mentores';
 
 //import database of members and activities from ccc-yt
-import  { miembros_CCCYT, activi_CCCYT } from '/imports/api/db_ccc_yt';
+import  { Club_CCCYT, Miembros_CCCYT, Actividad_CCCYT} from '/imports/api/db_ccc_yt';
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -42,6 +42,42 @@ Meteor.startup(() => {
 
     json.forEach(function(item){
       Actividad_Mentores.insert(item)
+    })
+  }
+
+  // Club Mentores
+  if (Club_CCCYT.find().count() === 0){
+    console.log("Importing private/club_CCCYT.json to db")
+
+    // read your file as a json string (assuming it's in the private dir)
+    var json = JSON.parse(Assets.getText('club_CCCYT.json'));
+
+    json.forEach(function(item){
+      Club_CCCYT.insert(item)
+    })
+  }
+
+  //MENTORES MIEMBROS
+  if (Miembros_CCCYT.find().count() === 0){
+    console.log("Importing private/miembros_CCCYT.json to db")
+
+    // read your file as a json string (assuming it's in the private dir)
+    var json = JSON.parse(Assets.getText('miembros_CCCYT.json'));
+
+    json.forEach(function(item){
+      Miembros_CCCYT.insert(item)
+    })
+  }
+  
+  //MENTORES ACTIVIDADES
+  if (Actividad_CCCYT.find().count() === 0){
+    console.log("Importing private/actividades_CCCYT.json to db")
+
+    // read your file as a json string (assuming it's in the private dir)
+    var json = JSON.parse(Assets.getText('actividades_CCCYT.json'));
+
+    json.forEach(function(item){
+      Actividad_CCCYT.insert(item)
     })
   }
 });
