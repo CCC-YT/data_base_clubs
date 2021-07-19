2ec5a4b717e79e2bd29257e596f8d6a256318093
Valerylu97 <65468343+Valerylu97@users.noreply.github.com>
2021-07-18 19:06:23 -0500

    commit mentores

---
+++ db_clubes_yt/imports/api/db_mentores.js
@@ -0,0 +1,158 @@
+import {Mongo} from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+SimpleSchema.extendOptions(['autoform']);
+
+/*Exportar esquemas*/
+export const Club_Mentores = new Mongo.Collection("club_mentores");
+export const Miembros_Mentores = new Mongo.Collection("miembros_mentores");
+export const Actividad_Mentores = new Mongo.Collection("actividad_mentores");
+
+
+/*Esquema Mentores */
+Club_Mentores.attachSchema(new SimpleSchema ({
+	_id:{
+		type: String,
+		label: 'Id club'
+	},
+	Nombre:{
+		type:String,
+		label: 'Nombre club',
+		required: true
+	},
+	Correo: {
+		type: String,
+		label: 'Email',
+		regEx: SimpleSchema.RegEx.Email,
+		autoform: {
+		   type: 'email'
+		},
+		required: false
+	 },
+	 Vision:{
+		type: String,
+		label: 'Visión',
+		required: false
+	 },
+	 Mision:{
+		type: String, 
+		label: 'Misión',
+		required: false
+	 },
+	 Objetivo:{
+		type: String,
+		label: 'Objetivo',
+		required: false
+	 }
+},
+{tracker: Tracker}
+));
+
+/*Esquema miembros mentores*/
+Miembros_Mentores.attachSchema(new SimpleSchema({
+	_id: {
+		type: String,
+		label: 'Id Miembro'
+	},
+	idClub:{
+		type: String,
+		//ref: 'Club_Mentores',
+		label: '_id Club',
+		required: true
+	},
+	Apellidos: {
+		type: String, 
+		label: 'Apellidos',
+		required: true,
+	},
+	Nombres: {
+		type: String,
+		label: 'Nombres',
+		required: true,
+	},
+	CI:{
+		type: String,
+		label: 'Cédula de identidad',  
+		required: false,
+	},
+	Correo:{
+		type: String,
+		label: 'Correo Electrónico',
+		required: true,
+	},
+	Carrera:{
+		type: String,
+		label: 'Carrera',
+		required:false,
+	},
+	Semestre:{
+		type: String,
+		label: 'Semestre',
+		required: false,
+	},
+	Comision:{
+		type: String,
+		label: 'Comisión',
+		required:false,
+	},
+	Cargo:{
+		type: String,
+		label: 'Cargo',
+		required:false,
+	},
+	Periodo:{
+		type: String,
+		label: 'Periodo',
+		required:false,
+	},
+	Estado:{
+		type: String,
+		label: 'Estado',
+		required: true,
+	}
+}, 
+{tracker: Tracker}
+));
+
+Actividad_Mentores.attachSchema(new SimpleSchema({
+	_id: {
+		type: String,
+		label: 'Id Actividad'
+	},
+	Nombre: {
+		type: String, 
+		label: 'Nombre de actividad',
+		required:true,
+	},
+	Tipo: {
+		type: String,
+		label: 'Tipo de actividad',
+		required: true,
+	},
+	Fecha_inicio: {
+		type: String,
+		label: 'Fecha de inicio de actividad',  
+		required: false,
+	},
+	idCoordinador:[{
+		type: String,
+		label: 'Id Coordinador de actividad',
+		required: false,
+	}],
+	idColaborador: [{
+		type: String,
+		label: 'Id Colaboradores de actividad',
+		required: false,
+	}],
+	Description:{
+		type: String,
+		label: 'Descripción actividad',
+		required: true,
+	},
+	Estado:{
+		type: String,
+		label: 'Estado de actividad',
+		required:true,
+	}
+},
+{tracker: Tracker}
+));
--- db_clubes_yt/server/main.js
+++ db_clubes_yt/server/main.js
@@ -1,5 +1,47 @@
 import { Meteor } from 'meteor/meteor';
 
+//import database of members and activities from mentors
+import  { Club_Mentores, Miembros_Mentores, Actividad_Mentores } from '/imports/api/db_mentores';
+
+//import database of members and activities from ccc-yt
+import  { miembros_CCCYT, activi_CCCYT } from '/imports/api/db_ccc_yt';
+
 Meteor.startup(() => {
   // code to run on server at startup
+  
+  // Club Mentores
+  if (Club_Mentores.find().count() === 0){
+    console.log("Importing private/club_mentores.json to db")
+
+    // read your file as a json string (assuming it's in the private dir)
+    var json = JSON.parse(Assets.getText('club_mentores.json'));
+
+    json.forEach(function(item){
+      Club_Mentores.insert(item)
+    })
+  }
+
+  //MENTORES MIEMBROS
+  if (Miembros_Mentores.find().count() === 0){
+    console.log("Importing private/miembros_mentores.json to db")
+
+    // read your file as a json string (assuming it's in the private dir)
+    var json = JSON.parse(Assets.getText('miembros_mentores.json'));
+
+    json.forEach(function(item){
+      Miembros_Mentores.insert(item)
+    })
+  }
+  
+  //MENTORES ACTIVIDADES
+  if (Actividad_Mentores.find().count() === 0){
+    console.log("Importing private/actividades_mentores.json to db")
+
+    // read your file as a json string (assuming it's in the private dir)
+    var json = JSON.parse(Assets.getText('actividades_mentores.json'));
+
+    json.forEach(function(item){
+      Actividad_Mentores.insert(item)
+    })
+  }
 });
